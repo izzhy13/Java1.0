0bc77fab4d91ade962c587fe13d3e7da94c02a54
EduDAMGalileo <eduardoj.niegar@educa.jcyl.es>
2025-12-21 22:55:52 +0100

    Cosas del Javadoc

---
+++ JavaBasico/doc/module-search-index.js
@@ -0,0 +1,1 @@
+moduleSearchIndex = [{"l":"JavaBasico"}];updateSearchResults();
---
+++ JavaBasico/doc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"basicos","l":"Documentacion"},{"p":"basicos","l":"Documentacion.GestorConexiones"}];updateSearchResults();
