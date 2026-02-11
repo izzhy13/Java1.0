31f2961253a3db8b63499d3f08667bb306243ba8
EduDAMGalileo <eduardoj.niegar@educa.jcyl.es>
2026-02-11 20:35:02 +0100

    Misery

--- src/tresEnRaya/AppJuegos.java
+++ src/tresEnRaya/AppJuegos.java
@@ -1,15 +1,40 @@
 package tresEnRaya;
 
+import java.util.Scanner;
+
 public class AppJuegos {
 
 	public static void main(String[] args) {
 		//Vamos a jugar al 3 en raya
+		Scanner sc = new Scanner(System.in);
+		int eleccion;
 		
-		TresEnRaya partida = new TresEnRaya("Edu", 'X', "Ana", 'O');
+		do {
+			System.out.println("¿A qué quieres jugar?");
+			System.out.println("1-Tres en raya");
+			System.out.println("2-Tres en raya miseria");
+			System.out.println("0-Salir");
+			eleccion=sc.nextInt();
+			
 		
-		partida.inicio();
+		}while(eleccion!=0);
+		
+		switch(eleccion) {
+		case 1:
+			TresEnRaya partida = new TresEnRaya("Edu", 'X', "Ana", 'O');
+			partida.inicio();
+			break;
+		case 2:
+			TresEnRayaMisery partida2 = new TresEnRayaMisery("Edu", 'X', "Ana", 'O');
+			partida2.inicio();
+			break;
+		case 0:
+			System.out.println("Vuelve pronto!");
+		}
+		
 		
 
 	}
 
 }
+
