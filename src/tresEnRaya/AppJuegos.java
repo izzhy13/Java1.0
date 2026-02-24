fa8a3daaa76d59a403ae90bfc186373843a11785
EduDAMGalileo <eduardoj.niegar@educa.jcyl.es>
2026-02-24 20:22:31 +0100

    herencia aplicada

--- src/tresEnRaya/AppJuegos.java
+++ src/tresEnRaya/AppJuegos.java
@@ -8,31 +8,32 @@ public class AppJuegos {
 		//Vamos a jugar al 3 en raya
 		Scanner sc = new Scanner(System.in);
 		int eleccion;
-		
+
 		do {
 			System.out.println("¿A qué quieres jugar?");
 			System.out.println("1-Tres en raya");
 			System.out.println("2-Tres en raya miseria");
 			System.out.println("0-Salir");
 			eleccion=sc.nextInt();
-			
-		
+
+
+			switch(eleccion) {
+			case 1:
+				System.out.println("Jugando al tres en raya");
+				TresEnRaya partida = new TresEnRaya("Edu", 'X', "Ana", 'O');
+				partida.inicio();
+				break;
+			case 2:
+				System.out.println("Jugando a la variante miseria");
+				TresEnRayaMisery partida2 = new TresEnRayaMisery("Edu", 'X', "Ana", 'O');
+				partida2.inicio();
+				break;
+			case 0:
+				System.out.println("Vuelve pronto!");
+			}
+
 		}while(eleccion!=0);
-		
-		switch(eleccion) {
-		case 1:
-			TresEnRaya partida = new TresEnRaya("Edu", 'X', "Ana", 'O');
-			partida.inicio();
-			break;
-		case 2:
-			TresEnRayaMisery partida2 = new TresEnRayaMisery("Edu", 'X', "Ana", 'O');
-			partida2.inicio();
-			break;
-		case 0:
-			System.out.println("Vuelve pronto!");
-		}
-		
-		
+
 
 	}
 
